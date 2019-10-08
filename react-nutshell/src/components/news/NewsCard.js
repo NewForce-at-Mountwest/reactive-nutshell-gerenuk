8cbe8fd0ffa90a5e6c01b9b59c55037d77e0313a
newforce <seaverstanley1408@gmail.com>
2019-10-08 11:17:43 -0400

    new master

--- react-nutshell/src/components/news/NewsCard.js
+++ react-nutshell/src/components/news/NewsCard.js
@@ -1,1 +1,24 @@
 
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./news.css";
+
+
+export default class NewsCard extends Component {
+  render() {
+
+    return (
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">{this.props.news.title}</h5>
+          <a target ="_blank" rel="noopener noreferrer" href={this.props.news.url} alt="news link">{this.props.news.url}</a>
+          <p>{this.props.news.synopsis}</p>
+        </div>
+            <Link className="nav-link mng-link" to={`/news/${this.props.news.id}`}>
+            Manage
+            </Link>
+
+        </div>
+    );
+  }
+}
